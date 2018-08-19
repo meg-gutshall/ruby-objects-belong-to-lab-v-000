8ebeeee2ad578a59f2ceea1aa2a6883fed98c43f
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-19 01:36:49 +0000

    First object relationships programlearn

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+# Create an Artist class
+class Artist
+  attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+# Create a Post class and let song instances relate to Author class instances
+class Post
+  attr_accessor :title, :author
+
+  def initialize
+    @title = title
+  end
+
+end
